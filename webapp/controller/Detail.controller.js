0b8418154494a6f8ad469e4b8a908ea765f0f154
José Rojas <22415328+rojasjo@users.noreply.github.com>
2024-05-30 12:54:17 +0200

    completed the tutorial

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/core/routing/History"
+],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, History) {
+    "use strict";
+
+    return Controller.extend("sap.training.exc.controller.Detail", {
+
+      onInit: function () {
+        var oRouter = this.getOwnerComponent().getRouter();
+        oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+      },
+      onNavBack: function () {
+        var oHistory = History.getInstance();
+        var sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          var oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("overview", {}, true);
+        }
+      },
+
+      _onObjectMatched: function (oEvent) {
+        this.getView().bindElement("/UX_Customer" + oEvent.getParameter("arguments").customerId);
+      }
+    });
+  });
---
+++ webapp/controller/Overview.controller.js
@@ -0,0 +1,75 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/syncStyleClass",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, syncStyleClass, JSONModel, Filter, FilterOperator, MessageToast) {
+        "use strict";
+
+        return Controller.extend("sap.training.exc.controller.Overview", {
+
+            onInit: function () {
+                var oModel = new JSONModel();
+                this.getView().setModel(oModel, "customer");
+            },
+
+            onSave: function () {
+                var oModelData = this.getView().getModel("customer").getData();
+                var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+                
+                if (oModelData.Discount === undefined) { oModelData.Discount = 0; }
+                
+                this.byId("customerTable").getBinding("items").create({
+                  "Form": oModelData.Form,
+                  "CustomerName": oModelData.CustomerName,
+                  "Discount": oModelData.Discount + "",
+                  "Street": oModelData.Street,
+                  "PostCode": oModelData.PostCode,
+                  "City": oModelData.City,
+                  "Country": oModelData.Country,
+                  "Email": oModelData.Email,
+                  "Telephone": oModelData.Telephone
+                }).created().then(function () {
+                  MessageToast.show(oResourceBundle.getText("customerCreatedMessage"));
+                });
+            },
+
+            onCloseDialog: function () {
+                this.byId("dialog").close();
+            },
+
+            onCustomerChange: function (oEvent) {
+                var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+                this.byId("bookingTable").setBindingContext(oBindingContext);
+            },
+
+            onFilterCustomers: function (oEvent) {
+                var aFilter = [];
+                var sQuery = oEvent.getParameter("query");
+                if (sQuery && sQuery.length > 0) {
+                  aFilter.push(new Filter("CustomerName", FilterOperator.Contains, sQuery));
+                }
+              
+                var oTable = this.byId("customerTable");
+                var oBinding = oTable.getBinding("items");
+                oBinding.filter(aFilter);
+              },
+
+              onNavToDetails: function (oEvent) {
+                var oItem = oEvent.getSource();
+                var oRouter = this.getOwnerComponent().getRouter();
+              
+              
+                oRouter.navTo("detail", {
+                  customerId: oItem.getBindingContext().getPath().substring("/UX_Customer".length)
+                });
+              }
+
+        });
+    });
